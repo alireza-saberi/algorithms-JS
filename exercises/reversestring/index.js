5c1a03a2522710b66bed9dc1acdf2cc0121f08f1
Alan Saberi <alan.saberi@nuance.com>
2019-04-17 13:44:52 -0400

    [#3] feat: adding second and third techniques for reversting a string
    these two are commented
    
    Changes to be committed:
    	modified:   exercises/reversestring/index.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -15,4 +15,32 @@ function reverse(str) {
     return str.split('').reverse().join('');
 }
 
+function reverse(str) {
+// there are three solutions available
+
+//    1st solution is built in solution
+//    reverse for arrays
+//    String > to array > reverse it > back to String
+    return str.split('').reverse().join('');
+}
+
+// 2nd approach with for each techinique
+
+// function reverse(str) {
+//   let reversed = '';
+//
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+
+// 3rd method
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+
 module.exports = reverse;
+
