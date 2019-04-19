43388c21526f796916df49bba679406d669b7815
Alan Saberi <saberi.ar.eng@gmail.com>
2019-04-19 12:31:52 -0400

    [#5] palindrome: using every helper function
    Changes to be committed:
    	modified:   exercises/palindrome/index.js

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,13 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
-    const reversed = str
-    .split('')
-    .reverse()
-    .join('');
+// function palindrome(str) {
+//     const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+//
+//   return str === reversed;
+// }
 
-  return str === reversed;
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
 }
 
 module.exports = palindrome;
