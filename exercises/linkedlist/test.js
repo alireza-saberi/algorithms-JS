d2de2cd8d94071560904d758c50919260b86a0c9
Alan Saberi <saberi.ar.eng@gmail.com>
2019-04-20 19:36:43 -0400

    [#22] insertFirst method of linkedlist/index very tricky one

--- exercises/linkedlist/test.js
+++ exercises/linkedlist/test.js
@@ -10,3 +10,20 @@ test('Node is a class', () => {
   expect(typeof Node.prototype.constructor).toEqual('function');
 });
 
+describe('A Node', () => {
+  test('has properties "data" and "next"', () => {
+    const node = new Node('a', 'b');
+    expect(node.data).toEqual('a');
+    expect(node.next).toEqual('b');
+  });
+});
+
+describe('Insert First', () => {
+  test('appends a node to the start of the list', () => {
+    const l = new List();
+    l.insertFirst(1);
+    expect(l.head.data).toEqual(1);
+    l.insertFirst(2);
+    expect(l.head.data).toEqual(2);
+  });
+});
