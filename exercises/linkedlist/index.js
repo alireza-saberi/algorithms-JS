b0ebecc15d2ce845d08812b39a5db050ff07b1df
Alan Saberi <saberi.ar.eng@gmail.com>
2019-04-20 19:59:14 -0400

    [#24] getFirst, getLast, and clear methods are implemented

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -29,6 +29,28 @@ class LinkedList {
 
         return counter;
     }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
+
+    clear() {
+        this.head = null;
+    }
 }
 
 module.exports = { Node, LinkedList };
