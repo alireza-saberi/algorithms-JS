1e42681cf138b8b3b3392c011f90b8dc406592b9
Alan Saberi <saberi.ar.eng@gmail.com>
2019-04-21 09:54:13 -0400

    [#28] insertAt implementation of the LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -104,6 +104,24 @@ class LinkedList {
         }
         return null;
     }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const previous = this.getAt(index - 1) || this.getLast();
+        const node = new Node(data, previous.next);
+        previous.next = node;
+    }
+
+    
 }
 
 module.exports = { Node, LinkedList };
