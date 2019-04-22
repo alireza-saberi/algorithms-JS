50b1354843ad03da8c95fc03213edddd7cdd3875
Alan Saberi <saberi.ar.eng@gmail.com>
2019-04-21 20:29:09 -0400

    [#32] tree: implementing node

---
+++ exercises/tree/test.js
@@ -0,0 +1,30 @@
+const T = require('./index');
+const Node = T.Node;
+const Tree = T.Tree;
+
+describe('Node', () => {
+  test('Node is a constructor', () => {
+    expect(typeof Node.prototype.constructor).toEqual('function');
+  });
+
+  test('Node has a data and children properties', () => {
+    const n = new Node('a');
+    expect(n.data).toEqual('a');
+    expect(n.children.length).toEqual(0);
+  });
+
+  test('Node can add children', () => {
+    const n = new Node('a');
+    n.add('b');
+    expect(n.children.length).toEqual(1);
+    expect(n.children[0].children).toEqual([]);
+  });
+
+  test('Node can remove children', () => {
+    const n = new Node('a');
+    n.add('b');
+    expect(n.children.length).toEqual(1);
+    n.remove('b');
+    expect(n.children.length).toEqual(0);
+  });
+});
